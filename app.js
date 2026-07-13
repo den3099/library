8e8c5079b193ff712002e9bcb72ee02cf90bc374
den3099 <dennisarce99@gmail.com>
2026-07-13 11:41:29 -0600

    Create main HTML structure and implement dynamic book card rendering

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const bookListSection = document.querySelector("#book-list");
+let bookCard = document.createElement("div");
+let title = document.createElement("p");
+
 const libraryBooks = [];
 
 function Book(author, title, pages, readStatus, id) {
@@ -16,6 +20,38 @@ function addBookToLibrary (author, title, pages, readStatus, id) {
     libraryBooks.push(book);
 }
 
+function displayBooks () {
+    bookListSection.textContent = "";
+    for (const book of libraryBooks) {
+        let bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+
+        bookCard.appendChild(createBookCardLines("Author: ", book.author));
+        bookCard.appendChild(createBookCardLines("Title: ", book.title));
+        bookCard.appendChild(createBookCardLines("Number of Pages: ", book.pages));
+        bookCard.appendChild(createBookCardLines("Read Status: ", book.readStatus));
+        bookCard.appendChild(createBookCardLines("ID: ", book.id));
+
+        bookListSection.appendChild(bookCard);
+    }
+}
+
+function createBookCardLines (cardLabel, bookInfo) {
+    let line = document.createElement("p");
+    let label = document.createElement("span");
+    let info = document.createElement("span");
+
+    label.classList.add("card-label");
+    label.textContent = cardLabel;
+    info.textContent = bookInfo;
+
+    line.appendChild(label);
+    line.appendChild(info);
+
+    return line;
+}
+
 addBookToLibrary("a", "b", "c", "d", "e");
 addBookToLibrary("f", "g", "h", "i", "j");
-console.log(libraryBooks);
+displayBooks();
+displayBooks();
